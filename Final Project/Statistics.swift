8ea2ddd4ef64e8f27e6a4a88d94d5e7ca863266d
Chris Matute <87210832+chrisitoo@users.noreply.github.com>
2024-05-05 21:32:51 -0400

    Add tab bar and initial view controllers

---
+++ Final Project/Statistics.swift
@@ -0,0 +1,8 @@
+//
+//  Statistics.swift
+//  Final Project
+//
+//  Created by Chris Matute on 5/5/24.
+//
+
+import Foundation
---
+++ Final Project/ViewControllers/HomeViewController.swift
@@ -0,0 +1,53 @@
+//
+//  HomeViewController.swift
+//  Final Project
+//
+//  Created by Chris Matute on 4/24/24.
+//
+
+import UIKit
+
+class HomeViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+    
+    @IBOutlet weak var tableView: UITableView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        tableView.delegate = self
+        tableView.dataSource = self
+        
+        
+    }
+    
+  /* func labelDesign () {
+        appName.font = UIFont(name: "Roboto-Regular", size: 50)
+        appName.backgroundColor = UIColor.red
+        appName.textAlignment = .center
+    }
+*/
+    
+    
+    // TableView
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 1
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        let cell = tableView.dequeueReusableCell(withIdentifier: "HomeItem" , for: indexPath)
+        
+        return cell
+    }
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
