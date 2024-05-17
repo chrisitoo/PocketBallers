26f0435419dbb1bc6001ea7568a3361bb43fdac5
Chris Matute <chrisamatute@gmail.com>
2024-05-17 01:26:41 -0400

    Expanded player model and successfully called and displayed data from API-Football on home tab.

---
+++ Final Project/PlayerInfo.swift
@@ -0,0 +1,144 @@
+//
+//  Football.swift
+//  Final Project
+//
+//  Created by Chris Matute on 4/24/24.
+//
+
+import Foundation
+
+struct Response: Codable {
+    let response: [PlayerData]
+}
+
+struct PlayerData: Codable {
+    let player: Player
+    let statistics: [PlayerStats]
+    let results: Int?
+}
+
+struct Player: Codable {
+    let age: Int
+    let birth: Birth
+    
+   
+    let firstname: String
+    let height: String
+    let id: Int
+    let injured: Bool
+    let lastname: String
+    let name: String
+    let nationality: String
+    let photo: URL
+    let weight: String
+}
+
+struct Birth: Codable {
+    let date: String
+    let place: String
+    let country: String
+}
+
+struct PlayerStats: Codable{
+    let cards: Cards
+    let dribbles: Dribbles
+    let duels: Duels
+    let fouls: Fouls
+    let games: Games
+    let goals: Goals
+    let league: League
+    let passes: Passes
+    let penalty: Penalty
+    let shots: Shots
+    let substitutes: Substitutes
+    let tackles: Tackles
+    let team: Team
+}
+
+struct Team: Codable {
+    let id: Int
+    let logo: URL
+    let name: String
+}
+
+struct League: Codable {
+    let id: Int
+    let name: String
+    let country: String
+    let logo: URL
+    let flag: URL
+    let season: Int
+}
+
+struct Games: Codable {
+    let appearances: Int?
+    let captain: Bool?
+    let lineups: Int?
+    let minutes: Int?
+    let number: Int?
+    let position: String?
+    let rating: String?
+    
+}
+
+struct Substitutes: Codable {
+    let bench: Int?
+    let `in`: Int?
+    let out: Int?
+}
+
+struct Shots: Codable {
+    let total: Int?
+    let on: Int?
+}
+
+struct Goals: Codable {
+    let total: Int?
+    let conceded: Int?
+    let assists: Int
+    let saves: Int?
+}
+
+struct Passes: Codable {
+    let total: Int?
+    let key: Int?
+    let accuracy: Int?
+}
+
+struct Tackles: Codable {
+    let blocks: Int?
+    let interceptions: Int?
+    let total: Int?
+}
+
+struct Duels: Codable {
+    let total: Int?
+    let won: Int?
+}
+
+struct Dribbles: Codable {
+    let attempts: Int?
+    let past: Int?
+    let success: Int?
+}
+
+struct Fouls: Codable {
+    let committed: Int?
+    let drawn: Int?
+}
+
+struct Cards: Codable {
+    let red: Int?
+    let yellow: Int?
+    let yellowred: Int?
+}
+
+struct Penalty: Codable {
+    let won: Int?
+    let committed: Int?
+    let scored: Int?
+    let missed: Int?
+    let saved: Int?
+}
+
+
--- Final Project/ViewControllers/HomeViewController.swift
+++ Final Project/ViewControllers/HomeViewController.swift
@@ -6,36 +6,78 @@
 //
 
 import UIKit
+import Foundation
+
 
 class HomeViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
     
+    
+    var playerResponse: Response?
     @IBOutlet weak var tableView: UITableView!
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
         tableView.delegate = self
         tableView.dataSource = self
         
+        let url = URL(string:"https://v3.football.api-sports.io/players/topscorers?league=140&season=2023")!
+        var request = URLRequest(url: url)
+        request.addValue("4fc176f9aa1841a75a883276b9a18aee", forHTTPHeaderField: "x-apisports-key")
+        request.httpMethod = "GET"
+        
+        
+        
+        let session = URLSession.shared
+        let dataTask = session.dataTask(with: request) {(data, response, error) in
+            if let error = error {
+                print("Failure: \(error.localizedDescription)")
+            } else {
+                if let data = data, let topScorers = self.parse(data:data) {
+                    self.playerResponse = topScorers
+                    
+                    DispatchQueue.main.async {
+                        self.tableView.reloadData()
+                    }
+                }
+            }
+            
+            
+        }
+        dataTask.resume()
         
     }
     
-  /* func labelDesign () {
-        appName.font = UIFont(name: "Roboto-Regular", size: 50)
-        appName.backgroundColor = UIColor.red
-        appName.textAlignment = .center
+    func parse(data:Data) -> Response? {
+        do {
+            let decoder = JSONDecoder()
+            let topScorers = try decoder.decode(Response.self, from: data)
+            return topScorers
+        } catch {
+            print("JSON error: \(error)")
+            return nil
+        }
     }
-*/
     
+    enum FootyError: Error {
+        case invalidURL
+        case invalidResponse
+        case invalidData
+    }
     
     // TableView
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 1
+        return playerResponse?.response.count ?? 0
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        
         let cell = tableView.dequeueReusableCell(withIdentifier: "HomeItem" , for: indexPath)
+       
+        var cellContent = cell.defaultContentConfiguration()
+    
+        cellContent.text = playerResponse?.response[indexPath.row].player.name
+        cell.contentConfiguration = cellContent
         
         return cell
     }
